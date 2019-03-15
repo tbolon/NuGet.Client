47868a333a7e436feb3423a6a69d85748edee403
Damon Tivel <dtivel@microsoft.com>
2019-03-15 12:19:20 -0700

    wip

--- test/EndToEnd/NuGet.Tests.psm1
+++ test/EndToEnd/NuGet.Tests.psm1
@@ -365,7 +365,7 @@ function Run-Test {
                                 Test = $name
                                 Error = $_
                             }
-                            Write-Host -ForegroundColor Red "$($testObject.InvocationInfo.InvocationName) Failed: $testObject. Exception message: $_"
+                            Write-Host -ForegroundColor Red "$($testObject.InvocationInfo.InvocationName) Failed: $testObject. Exception message: $_  Callstack:`r`n$($PSItem.ScriptStackTrace)"
                             $testSucceeded = $false
                         }
                     }
